e9176ca576db9a4b6bf727a3da594ab7f49a7a04
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2020-05-13 15:29:41 +0100

    Removing unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client side javascript is loadeddd!!')
-
-
 // http://puzzle.mead.io/puzzle
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -15,15 +12,12 @@ weatherForm.addEventListener('submit', (e) =>{
     fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location) ).then((response) => {
     response.json().then((data) =>{
         if (data.error) {
-            console.log(data.error);
             messageOne.textContent =  data.error
         }else{
             console.log(data);
             messageOne.textContent = data.location
-            // messageTwo.textContent = 'Address: ' + data.Address + ' Location: ' + data.location + ' Forecast:' +data.Forecast
             messageTwo.textContent = data.Forecast
         }
-        
     })
     })
 })
